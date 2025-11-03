1bb490719ed86580a0e2dc04cb2338cc82569fcd
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-04 01:42:36 +0900

    only type

--- dist/arena/season_1/portal_exploration/basic/prototypes/portal.d.ts
+++ dist/arena/season_1/portal_exploration/basic/prototypes/portal.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../../../../game/prototypes/index.d.ts" />
 declare module "arena/season_1/portal_exploration/basic/prototypes" {
-    import { _Constructor, GameObject } from "game/prototypes";
+    import type { _Constructor, GameObject } from "game/prototypes";
 
     /** A portal is a special type of game object that allows creeps to teleport */
     export interface Portal extends GameObject {

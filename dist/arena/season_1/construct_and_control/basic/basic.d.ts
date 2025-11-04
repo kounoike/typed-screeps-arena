8210d3485b0f104ebc47656fbe93a33641dff3fc
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-04 15:37:32 +0900

    cnc

--- dist/arena/season_1/construct_and_control/basic/basic.d.ts
+++ dist/arena/season_1/construct_and_control/basic/basic.d.ts
@@ -1,6 +1,4 @@
-/// <reference path="./prototypes/area-effect.d.ts" />
-/// <reference path="./prototypes/construction-boost.d.ts" />
-/// <reference path="./prototypes/structure-goal.d.ts" />
+/// <reference path="./prototypes/index.d.ts" />
 /// <reference path="./constants.d.ts" />
 declare module "arena/season_1/construct_and_control/basic" {
     export * from "arena/season_1/construct_and_control/basic/prototypes";
---
+++ dist/arena/season_1/construct_and_control/basic/prototypes/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./area-effect.d.ts" />
+/// <reference path="./construction-boost.d.ts" />
+/// <reference path="./structure-goal.d.ts" />

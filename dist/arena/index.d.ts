f7f8bee575878610b3aa7edcf421f857216d1bd6
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-10 00:32:22 +0900

    still needs by constants

--- dist/arena/index.d.ts
+++ dist/arena/index.d.ts
@@ -1,8 +1,7 @@
-// commented out old definitions
-// /// <reference path="prototypes/index.d.ts" />
-// /// <reference path="constants.d.ts" />
+/// <reference path="prototypes/index.d.ts" />
+/// <reference path="constants.d.ts" />
 
-// declare module "arena" {
-//     export * from "arena/prototypes";
-//     export * from "arena/constants";
-// }
+declare module "arena" {
+    export * from "arena/prototypes";
+    export * from "arena/constants";
+}

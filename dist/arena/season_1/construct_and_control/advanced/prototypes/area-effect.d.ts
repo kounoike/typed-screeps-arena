1477b84e041dfea832bc39aa15f6a3d21cc4fa0c
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-10 00:14:33 +0900

    construction and control advanced

--- dist/arena/season_1/construct_and_control/advanced/prototypes/area-effect.d.ts
+++ dist/arena/season_1/construct_and_control/advanced/prototypes/area-effect.d.ts
@@ -1,6 +1,11 @@
 declare module "arena/season_1/construct_and_control/advanced/prototypes" {
     import { _Constructor, GameObject } from "game/prototypes";
-    import { EFFECT_SLOWDOWN } from "arena/season_1/construct_and_control/advanced/constants";
+    import {
+        EFFECT_SLOWDOWN,
+        KIND_BLUE,
+        KIND_GREEN,
+        KIND_RED,
+    } from "arena/season_1/construct_and_control/advanced/constants";
 
     type AreaEffectType = typeof EFFECT_SLOWDOWN;
 
@@ -8,6 +13,7 @@ declare module "arena/season_1/construct_and_control/advanced/prototypes" {
     export interface AreaEffect extends GameObject {
         /** The effect type */
         readonly effect: AreaEffectType;
+        readonly kind: typeof KIND_BLUE | typeof KIND_GREEN | typeof KIND_RED;
     }
     export const AreaEffect: _Constructor<AreaEffect>;
 }

b61eeb47dfbb114890d8a688d1d84a52c1399874
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-10 02:53:37 +0900

    no class, interface

--- dist/arena/season_1/construct_and_control/basic/prototypes/construction-boost.d.ts
+++ dist/arena/season_1/construct_and_control/basic/prototypes/construction-boost.d.ts
@@ -1,12 +1,8 @@
-declare module "game/prototypes" {
-    export class GameObject {}
-}
-
 declare module "arena/season_1/construct_and_control/basic/prototypes" {
     import { GameObject } from "game/prototypes";
 
     /** An object that provides a construction boost effect to the creep that steps onto this object for 200 ticks */
-    export class ConstructionBoost extends GameObject {
+    export interface ConstructionBoost extends GameObject {
         /** The number of ticks until this object disappears */
         ticksToDecay: number;
     }

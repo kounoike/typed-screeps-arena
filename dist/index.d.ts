77b8aee40d0588bf8a2705521e3379640465eaaf
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2025-11-14 12:34:10 +0900

    need reference?

---
+++ dist/index.d.ts
@@ -0,0 +1,18 @@
+// Project: https://github.com/screepers/typed-screeps-arena
+// Definitions by: thmsn <https://github.com/thmsndk>
+//                 Skyler Kehren <https://github.com/pyrodogg>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.8
+
+/// <reference path="game/index.d.ts" />
+/// <reference path="arena/index.d.ts" />
+/// <reference path="arena/season_alpha/capture_the_flag/basic.d.ts" />
+/// <reference path="arena/season_alpha/capture_the_flag/advanced.d.ts" />
+/// <reference path="arena/season_alpha/collect_and_control/basic.d.ts" />
+/// <reference path="arena/season_alpha/collect_and_control/advanced.d.ts" />
+/// <reference path="arena/season_beta/capture_the_flag/basic.d.ts" />
+/// <reference path="arena/season_beta/capture_the_flag/advanced.d.ts" />
+/// <reference path="arena/season_beta/collect_and_control/basic.d.ts" />
+/// <reference path="arena/season_beta/collect_and_control/advanced.d.ts" />
+/// <reference path="arena/season_1/construct_and_control/basic/basic.d.ts" />
+/// <reference path="arena/season_1/portal_exploration/basic/basic.d.ts" />

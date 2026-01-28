e5f387b601a3260c2ea82293ff08509aa0e084e2
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2026-01-29 08:40:56 +0900

    型定義と実体定義

--- dist/arena/season_1/construct_and_control/advanced/prototypes/construction-boost.d.ts
+++ dist/arena/season_1/construct_and_control/advanced/prototypes/construction-boost.d.ts
@@ -1,8 +1,10 @@
-import { GameObject } from "game/prototypes";
+import { _Constructor, GameObject } from "game/prototypes";
 declare module "arena/season_1/construct_and_control/advanced/prototypes" {
     /** An object that provides a construction boost effect to the creep that steps onto this object for 200 ticks */
     export interface ConstructionBoost extends GameObject {
         /** The number of ticks until this object disappears */
         ticksToDecay: number;
     }
+
+    export const ConstructionBoost: _Constructor<ConstructionBoost>;
 }
--- dist/arena/season_1/construct_and_control/basic/prototypes/construction-boost.d.ts
+++ dist/arena/season_1/construct_and_control/basic/prototypes/construction-boost.d.ts
@@ -1,9 +1,11 @@
 declare module "arena/season_1/construct_and_control/basic/prototypes" {
-    import { GameObject } from "game/prototypes";
+    import { _Constructor, GameObject } from "game/prototypes";
 
     /** An object that provides a construction boost effect to the creep that steps onto this object for 200 ticks */
     export interface ConstructionBoost extends GameObject {
         /** The number of ticks until this object disappears */
         ticksToDecay: number;
     }
+
+    export const ConstructionBoost: _Constructor<ConstructionBoost>;
 }

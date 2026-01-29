9acd6970814af9059134e75311b639852e373df9
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2026-01-29 09:30:34 +0900

    no arena/season_1/construct_and_control/basic module

--- dist/arena/season_1/construct_and_control/advanced/advanced.d.ts
+++ dist/arena/season_1/construct_and_control/advanced/advanced.d.ts
@@ -1,9 +1,2 @@
 /// <reference path="./prototypes/index.d.ts" />
 /// <reference path="./constants.d.ts" />
-declare module "arena/season_1/construct_and_control/advanced" {
-    export { StructureGoal, ConstructionBoost } from "arena/season_1/construct_and_control/advanced/prototypes";
-    export {
-        EFFECT_SLOWDOWN,
-        EFFECT_CONSTRUCTION_BOOST,
-    } from "arena/season_1/construct_and_control/advanced/constants";
-}
--- dist/arena/season_1/construct_and_control/basic/basic.d.ts
+++ dist/arena/season_1/construct_and_control/basic/basic.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="./prototypes/index.d.ts" />
 /// <reference path="./constants.d.ts" />
-declare module "arena/season_1/construct_and_control/basic" {
-    export { StructureGoal, ConstructionBoost } from "arena/season_1/construct_and_control/basic/prototypes";
-    export { EFFECT_SLOWDOWN, EFFECT_CONSTRUCTION_BOOST } from "arena/season_1/construct_and_control/basic/constants";
-}

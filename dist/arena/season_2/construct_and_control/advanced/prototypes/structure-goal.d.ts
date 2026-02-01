b3573bda0ce2b9c6d828b13a9bdbb647fd1320d9
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2026-02-02 03:19:01 +0900

    season2 C&C

---
+++ dist/arena/season_2/construct_and_control/advanced/prototypes/structure-goal.d.ts
@@ -0,0 +1,13 @@
+import { _Constructor, _ConstructorById, OwnedStructure } from "game/prototypes";
+
+declare module "arena/season_1/construct_and_control/advanced/prototypes" {
+    export type STRUCTURE_GOAL = "goal";
+
+    /**
+     * A StructureGoal is a structure that serves as an objective in certain arena scenarios.
+     */
+    export interface StructureGoal extends OwnedStructure<STRUCTURE_GOAL> {}
+    interface StructureGoalConstructor extends _Constructor<StructureGoal>, _ConstructorById<StructureGoal> {}
+
+    export const StructureGoal: StructureGoalConstructor;
+}

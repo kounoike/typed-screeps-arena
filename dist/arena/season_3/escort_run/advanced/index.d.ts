147ee05ffa4eec1f169ab6eea413807a436af927
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2026-09-12 19:20:05 +0900

    add EscortCreep in arena/season_X

---
+++ dist/arena/season_3/escort_run/advanced/index.d.ts
@@ -0,0 +1,5 @@
+import { Creep } from "game/prototypes";
+
+declare module "arena/season_4/escort_run/advanced" {
+    export interface EscortCreep extends Creep {}
+}
---
+++ dist/arena/season_4/escort_run/basic/index.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_4/escort_run/basic" {
+    import { Creep } from "game/prototypes";
+    export interface EscortCreep extends Creep {}
+}
